708f631bfd134c9cd575a3a3f535cb10b0950851
shubug1015 <shubug@naver.com>
2022-11-15 18:32:36 +0900

    data fetch

--- pages/picture.tsx
+++ pages/picture.tsx
@@ -1,12 +1,13 @@
-import type { NextPage } from "next";
-import Image from "next/image";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Picture: NextPage = () => {
   const router = useRouter();
   const [preview, setPreview] = useState(null);
-  const [img, setImg] = useState(null);
+  const [imgFile, setImgFile] = useState(null);
+  const [data, setData] = useState<any>(null);
 
   const handlePhoto = (e: any) => {
     e.preventDefault();
@@ -16,81 +17,83 @@ const Picture: NextPage = () => {
     if (file) {
       reader.onloadend = () => {
         setPreview(reader.result);
-        setImg(file);
+        setImgFile(file);
       };
       reader.readAsDataURL(file);
     }
   };
 
-  const onclick = () => {
+  const getData = () => {
     const formData = new FormData();
-    formData.append("image", file);
-    fetch("http://localhost:5000/predict", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
+    formData.append('image', imgFile as any);
+
+    try {
+      fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          console.log(data);
+        });
+
+      router.push(`/predict?prediction_result=${data?.prediction_result}`);
+    } catch {
+      alert('Error');
+    }
   };
 
   return (
-    <div className="mx-auto flex h-screen max-w-[330px] flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">사진 분석</h1>
-      <div className="mt-12 aspect-square w-full">
+    <div className='mx-auto flex h-screen max-w-[330px] flex-col items-center justify-center'>
+      <h1 className='text-2xl font-bold'>사진 분석</h1>
+      <div className='mt-12 aspect-square w-full'>
         {preview ? (
-          <div className="relative h-full w-full">
+          <div className='relative h-full w-full'>
             <Image
               src={preview}
-              alt="Photo"
-              layout="fill"
-              objectFit="contain"
-              className="rounded-md"
+              alt='Photo'
+              layout='fill'
+              objectFit='contain'
+              className='rounded-md'
             />
           </div>
         ) : (
           <>
             <label
-              htmlFor="image"
-              className="flex h-full w-full items-center justify-center rounded-md bg-[#4a4e57] transition-all hover:opacity-90"
+              htmlFor='image'
+              className='flex h-full w-full items-center justify-center rounded-md bg-[#4a4e57] transition-all hover:opacity-90'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
                 strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-8 w-8"
+                stroke='currentColor'
+                className='h-8 w-8'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12 4.5v15m7.5-7.5h-15'
                 />
               </svg>
             </label>
             <input
-              type="file"
-              id="image"
+              type='file'
+              id='image'
               onChange={handlePhoto}
-              className="hidden"
+              className='hidden'
             />
           </>
         )}
       </div>
       <button
         className={
-          "mt-8 flex h-[3.688rem] w-full cursor-pointer items-center justify-center rounded bg-[#00e7ff] text-lg font-medium text-[#282e38] transition-all hover:opacity-90 md:h-14 md:text-base"
+          'mt-8 flex h-[3.688rem] w-full cursor-pointer items-center justify-center rounded bg-[#00e7ff] text-lg font-medium text-[#282e38] transition-all hover:opacity-90 md:h-14 md:text-base'
         }
-        type="button"
-        onClick={() =>
-          router.push({
-            pathname: "/predict",
-            query: { imageUrl: img },
-          })
-        }
+        type='button'
+        onClick={getData}
       >
         예측하기
       </button>
--- pages/predict.tsx
+++ pages/predict.tsx
@@ -1,69 +1,70 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const Predict: NextPage = () => {
   const router = useRouter();
-  const image = router.query.imageUrl as string;
+  const { prediction_result } = router.query;
+  // const image = router.query.imageUrl as string;
 
-  const formData = new FormData();
-  formData.append("image", image);
+  // const formData = new FormData();
+  // formData.append("image", image);
 
-  const [data, setData]: any = useState(null);
+  // const [data, setData]: any = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/predict", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/predict", {
+  //     method: "POST",
+  //     body: formData,
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //     });
+  // }, []);
 
   return (
-    <div className="mx-auto w-full pt-24 md:max-w-[330px]">
-      <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg font-medium leading-6 text-gray-900"></h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            {data && data.prediction_result}
+    <div className='mx-auto w-full pt-24 md:max-w-[330px]'>
+      <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
+        <div className='px-4 py-5 sm:px-6'>
+          <h3 className='text-lg font-medium leading-6 text-gray-900'></h3>
+          <p className='mt-1 max-w-2xl text-sm text-gray-500'>
+            {prediction_result}
           </p>
         </div>
-        <div className="border-t border-gray-200">
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Full name</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {data && data.price}
+        <div className='border-t border-gray-200'>
+          <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+            <dt className='text-sm font-medium text-gray-500'>Full name</dt>
+            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+              {/* {data && data.price} */}
             </dd>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
+          <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+            <dt className='text-sm font-medium text-gray-500'>
               Application for
             </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {data && data.price}
+            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+              {/* {data && data.price} */}
             </dd>
           </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Email address</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {data && data.price}
+          <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+            <dt className='text-sm font-medium text-gray-500'>Email address</dt>
+            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+              {/* {data && data.price} */}
             </dd>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
+          <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+            <dt className='text-sm font-medium text-gray-500'>
               Salary expectation
             </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {data && data.prediction_result}
+            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
+              {/* {data && data.prediction_result} */}
             </dd>
           </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+          <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+            <dt className='text-sm font-medium text-gray-500'>About</dt>
+            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
               Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
               incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
               consequat sint. Sit id mollit nulla mollit nostrud in ea officia
