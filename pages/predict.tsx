a898232302804fe3d323d6604ceaf692b310eee4
shubug1015 <shubug@naver.com>
2022-11-15 18:43:01 +0900

    router query

--- pages/predict.tsx
+++ pages/predict.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 const Predict: NextPage = () => {
   const router = useRouter();
-  const { prediction_result } = router.query;
+  const { prediction_result, price } = router.query;
   // const image = router.query.imageUrl as string;
 
   // const formData = new FormData();
@@ -30,14 +30,14 @@ const Predict: NextPage = () => {
         <div className='px-4 py-5 sm:px-6'>
           <h3 className='text-lg font-medium leading-6 text-gray-900'></h3>
           <p className='mt-1 max-w-2xl text-sm text-gray-500'>
-            {prediction_result}
+            {prediction_result && prediction_result}
           </p>
         </div>
         <div className='border-t border-gray-200'>
           <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <dt className='text-sm font-medium text-gray-500'>Full name</dt>
             <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
-              {/* {data && data.price} */}
+              {price && price}
             </dd>
           </div>
           <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
